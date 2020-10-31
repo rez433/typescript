e45e7783ce5ba2dea3556979c26a08c51e5d3896
rez433 <r3zaee@gmail.com>
2020-10-31 13:39:43 +0100

    Access Modifiers: readonly, public, private

--- basics-form/public/basics-form/public/app.js
+++ basics-form/public/basics-form/public/app.js
@@ -1,7 +1,45 @@
 "use strict";
-// const anchor = document.querySelector('a'); // gives error "Object is possible 'null'"
-// const anchor = document.querySelector('a')!;
-// console.log(anchor.href);
+// class Invoice {
+//     // client: string;
+//     // details: string;
+//     // amount: number;
+//     /* access modifiers readonly, private, public */
+//     readonly client: string; // can be accessed but not modified outside of class
+//     private details: string; // cant be accessed outside of class
+//     public amount: number; // can be accessed and modified outside of class
+//     constructor(c: string,d: string,a: number) {
+//         this.client = c;
+//         this.details = d;
+//         this.amount = a;
+//     }
+//     printInvoice() {
+//         return `${this.client} ows $${this.amount} for ${this.details}`
+//     }
+// }
+// class Invoice can be rewritten as:
+var Invoice = /** @class */ (function () {
+    function Invoice(client, details, amount) {
+        this.client = client;
+        this.details = details;
+        this.amount = amount;
+    }
+    Invoice.prototype.printInvoice = function () {
+        return this.client + " ows $" + this.amount + " for " + this.details;
+    };
+    return Invoice;
+}());
+var invOne = new Invoice('mario', 'work on website', 230);
+var invTwo = new Invoice('luigi', 'work on blog', 320);
+// console.log(invOne, invTwo);
+var invoices = []; // onlly allow Invoice objects to be pushed inside invoices list
+invoices.push(invOne);
+invoices.push(invTwo);
+console.log(invoices);
+// invOne.client = 'John Doe'; // givs error because client is readonly 
+invTwo.amount = 600;
+invoices.forEach(function (inv) {
+    console.log(inv.client, /*inv.details givs error because details is private */ inv.amount, inv.printInvoice()); //inv.printInvoice can access all properties even private ones
+});
 /* type casting */
 var form = document.querySelector('.new-item-form');
 // console.log(form.children); // prints all form-children with div tag
--- basics-form/public/basics-form/src/app.ts
+++ basics-form/public/basics-form/src/app.ts
@@ -1,10 +1,60 @@
-// const anchor = document.querySelector('a'); // gives error "Object is possible 'null'"
+// class Invoice {
+//     // client: string;
+//     // details: string;
+//     // amount: number;
 
-// const anchor = document.querySelector('a')!;
+//     /* access modifiers readonly, private, public */
+//     readonly client: string; // can be accessed but not modified outside of class
+//     private details: string; // cant be accessed outside of class
+//     public amount: number; // can be accessed and modified outside of class
 
-// console.log(anchor.href);
+//     constructor(c: string,d: string,a: number) {
+//         this.client = c;
+//         this.details = d;
+//         this.amount = a;
+//     }
+
+//     printInvoice() {
+//         return `${this.client} ows $${this.amount} for ${this.details}`
+//     }
+// }
 
 
+
+// class Invoice can be rewritten as:
+class Invoice {
+    constructor(
+        readonly client: string,
+        private details: string,
+        public amount: number
+    ){}
+
+
+    printInvoice() {
+        return `${this.client} ows $${this.amount} for ${this.details}`
+    }
+
+}
+
+
+const invOne = new Invoice('mario', 'work on website', 230);
+const invTwo = new Invoice('luigi', 'work on blog', 320);
+
+
+// console.log(invOne, invTwo);
+
+let invoices: Invoice[] = []; // onlly allow Invoice objects to be pushed inside invoices list
+invoices.push(invOne);
+invoices.push(invTwo);
+
+console.log(invoices);
+
+// invOne.client = 'John Doe'; // givs error because client is readonly 
+invTwo.amount = 600;
+
+invoices.forEach(inv => {
+    console.log(inv.client, /*inv.details givs error because details is private */ inv.amount, inv.printInvoice()) //inv.printInvoice can access all properties even private ones
+})
 
 /* type casting */
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
