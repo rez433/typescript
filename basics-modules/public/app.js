abe0ae3b00d4554f9a44dae82603f824f5f5c2a1
rez433 <r3zaee@gmail.com>
2020-10-31 16:58:49 +0100

    Basics Modules

---
+++ basics-modules/public/app.js
@@ -0,0 +1,24 @@
+import { Invoice } from './classes/Invoice.js';
+const invOne = new Invoice('mario', 'work on website', 230);
+const invTwo = new Invoice('luigi', 'work on blog', 320);
+// console.log(invOne, invTwo);
+let invoices = []; // onlly allow Invoice objects to be pushed inside invoices list
+invoices.push(invOne);
+invoices.push(invTwo);
+console.log(invoices);
+// invOne.client = 'John Doe'; // givs error because client is readonly 
+invTwo.amount = 600;
+invoices.forEach(inv => {
+    console.log(inv.client, /*inv.details givs error because details is private */ inv.amount, inv.printInvoice()); //inv.printInvoice can access all properties even private ones
+});
+/* type casting */
+const form = document.querySelector('.new-item-form');
+// console.log(form.children); // prints all form-children with div tag
+const type = document.querySelector('#type');
+const tofrom = document.querySelector('#tofrom');
+const details = document.querySelector('#details');
+const amount = document.querySelector('#amount');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+});
